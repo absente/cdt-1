524ecad74c92d8f9af86680a1606969f6272b2af
denghaihui <denghaihui@jiemo.me>
2016-09-22 23:42:47 +0800

    commit local

--- cdt/cdtAction/models.py
+++ cdt/cdtAction/models.py
@@ -1,8 +1,37 @@
 from __future__ import unicode_literals
-from django.db import models
+import random
+
+from django.utils import timezone
 from django.utils.encoding import python_2_unicode_compatible
-from django.contrib.contenttypes.fields import GenericRel
-from django.contrib.contenttypes.models import ContentType
+
+from django.db import models
+from django.contrib.auth.models import User
+
+from django.contrib.auth import authenticate
+
+
+@python_2_unicode_compatible
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    nikename = models.CharField(max_length=255, help_text='nike name')
+    email = models.EmailField(max_length=255, help_text='email')
+    mobile_no = models.CharField(max_length=255, help_text='mobile_no')
+
+    def __str__(self):
+        fileds = [self.user, self.email]
+
+        return ' '.join([str(r) for r in fileds])
+
+    def create_account(self):
+        while True:
+            dt = timezone.now()
+            username = '{}{}'.format(int(dt.timestamp()), random.randint(0, 1000))
+            check_sum = sum(int(r) for r in username) % 10
+            if check_sum != 0:
+                check_sum = 10 - check_sum
+            user, is_user = User.objects.get_or_create(username='{}{}'.format(username, check_sum))
+            if is_user:
+                return user
 
 
 @python_2_unicode_compatible
